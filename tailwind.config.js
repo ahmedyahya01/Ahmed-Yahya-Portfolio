49d23df33b9d39f73e14b456bc1997f5c7aa106f
Ahmed <ahmedlkhateb@gmail.com>
2025-03-14 17:35:09 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1400px",
+    },
+    extend: {
+      colors: {
+        sectionColor: "hsl(209, 100%, 97%)",
+        darkBodyColor: "hsl(216, 100%, 4%)",
+        darkSectionColor: "hsl(211, 100%, 12%)",
+        primaryColor: "hsl(209, 87%, 21%)",
+        primaryColorLight: "hsl(209, 74%, 45%)",
+        whiteColor: "#fff",
+        textColor: "#DDD",
+        secondaryColor: "red",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "10px",
+        md: "30px",
+      },
+    },
+    fontFamily: {
+      poppins: ["Poppins", "serif"],
+      londrina: ["Londrina Outline", "serif"],
+    },
+  },
+  plugins: [],
+};
